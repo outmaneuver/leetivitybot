a09adb84d29f31d4b806f2e5cc18b46ba37b086b
Cameron <cath1851@colorado.edu>
2024-10-23 22:39:40 -0600

    Setting up project framework. Initial commit

---
+++ src/leetcode.js
@@ -0,0 +1,37 @@
+const fetch = require('node-fetch');
+const config = require('./config');
+
+// Calls GraphQL API to query daily challenge question.
+async function fetchDailyProblem() {
+  const query = `
+    query questionOfToday {
+      activeDailyCodingChallengeQuestion {
+        date
+        userStatus
+        link
+        question {
+          title
+          difficulty
+          acRate
+          likes
+          dislikes
+        }
+      }
+    }
+  `;
+
+  const response = await fetch(config.LEETCODE_GRAPHQL_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  });
+
+  const data = await response.json();
+  return data.data.activeDailyCodingChallengeQuestion;
+}
+
+module.exports = {
+  fetchDailyProblem,
+};
